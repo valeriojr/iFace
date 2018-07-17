cbae56b99b4ef6a7412b2e5d5afe92fdd5edb3a8
Valério <vnrj@ic.ufal.br>
2018-07-16 21:00:37 -0300

    Solicitação de amizade

--- iFace.java
+++ iFace.java
@@ -12,6 +12,7 @@ public class iFace{
 	private static final int MAX_CMNT = 100;
 	private static final int MAX_FRIENDS = 100;
 	private static final int MAX_MESSAGES = 100;
+	private static final int MAX_RQST = 100;
 	
 	private static final int ACCT_COL = 3;
 	private static final int ACCT_EMAIL = 0;
@@ -28,15 +29,25 @@ public class iFace{
 	private static final int CMNT_CREATOR = 2;
 	
 	private static final int FRIEND_COL = 1;
-	private static final int FRIEND_NAME = 0;
+	private static final int FRIEND_EMAIL = 0;
 	
 	private static final int MESSAGES_COL = 1;
 	private static final int MESSAGE_SRC = 0;
+	
+	private static final int RQST_COL = 3;
+	private static final int RQST_SRC = 0;
+	private static final int RQST_DEST = 1;
+	private static final int RQST_TYPE = 2;
+	private static final int RQST_INVITE = 0;
+	private static final int RQST_INVITE_FAILURE = 1;
+	private static final int RQST_INVITE_SUCCESS = 2;
+	private static final int RQST_ACCOUNT_CLOSURE = 3;
 	//</Constants>
 	
 	//<Variables/>
 	private static String accounts[][];
 	private static String communities[][];
+	private static String requests[][];
 	private static String currUserAcct[];
 	private static String currUserAttr[][];
 	private static String currUserCmnt[][];
@@ -49,6 +60,7 @@ public class iFace{
 		
 		accounts = new String[MAX_ACCT][ACCT_COL];
 		communities = new String[MAX_CMNT][CMNT_COL];
+		requests = new String[MAX_RQST][RQST_COL];
 
 		currUserAcct = new String[ACCT_COL];
 		currUserAttr = new String[MAX_ATTR][ATTR_COL];
@@ -58,6 +70,7 @@ public class iFace{
 		
 		loadDatabase("accounts.txt", accounts, ACCT_COL);
 		loadDatabase("communities.txt", communities, CMNT_COL);
+		loadDatabase("requests.txt", requests, RQST_COL);
 		
 		printDatabase(accounts, ACCT_COL, MAX_ACCT);
 		clearScreen();
@@ -68,6 +81,7 @@ public class iFace{
 		
 		saveDatabase("accounts.txt", accounts, ACCT_COL, MAX_ACCT);
 		saveDatabase("communities.txt", communities, CMNT_COL, MAX_CMNT);
+		saveDatabase("requests.txt", requests, RQST_COL, MAX_RQST);
 		
 		input.close();
 	}
@@ -79,7 +93,7 @@ public class iFace{
 		loadDatabase(currUserFolder + "friends.txt", currUserFriends, FRIEND_COL);
 		loadDatabase(currUserFolder + "messages.txt", currUserMessages, MESSAGES_COL);
 		
-		String options[] = {"Meu perfil", "Comunidades", "Voltar"};
+		String options[] = {"Meu perfil", "Comunidades", "Amigos", "Voltar"};
 		clearScreen();
 		while(true){
 			showTitle("Início");
@@ -89,6 +103,9 @@ public class iFace{
 				break;
 			case 2:
 				communityMenu();
+				break;
+			case 3:
+				friendsMenu();
 				break;
 			default:
 				saveDatabase(currUserFolder + "attr.txt", currUserAttr, ATTR_COL, MAX_ATTR);
@@ -122,7 +139,6 @@ public class iFace{
 			}
 		}
 	}
-	
 	private static void showProfile(){
 		clearScreen();
 		showTitle("Meu perfil");
@@ -136,7 +152,6 @@ public class iFace{
 		}
 		getchar();
 	}
-	
 	private static void editProfileMenu(){
 		String options[] = {"Adicionar atributo", "Editar atributo", "Apagar atributo", "Voltar"};
 		while(true){
@@ -158,7 +173,6 @@ public class iFace{
 			
 		}
 	}
-	
 	private static void addAttribute(){
 		clearScreen();
 		showTitle("Adicionar atributo");
@@ -182,7 +196,6 @@ public class iFace{
 			}
 		}
 	}
-	
 	private static void editAttribute(){
 		clearScreen();
 		showTitle("Editar atributo");
@@ -195,7 +208,6 @@ public class iFace{
 		System.out.println("Atributo não encontrado");
 		getchar();
 	}
-	
 	private static void eraseAttribute(){
 		clearScreen();
 		showTitle("Apagar atributo");
@@ -209,7 +221,6 @@ public class iFace{
 		System.out.println("O atributo não existe");
 		getchar();
 	}
-	
 	//</Profile>
 	
 	//<Login/>	
@@ -229,6 +240,7 @@ public class iFace{
 				return;
 			}
 			if(currUserAcct[ACCT_EMAIL] != null){
+				solveRequests();
 				homeMenu();
 			}
 		}
@@ -252,11 +264,10 @@ public class iFace{
 			}
 		}
 		else{
-			System.out.println("Não existe nehuma conta associada a <" + email + ">");
+			System.out.println(index + "Não existe nehuma conta associada a <" + email + ">");
 			getchar();
 		}
 	}
-	
 	private static void signUp(){
 		clearScreen();
 		showTitle("Criar nova conta");
@@ -284,14 +295,13 @@ public class iFace{
 			getchar();
 		}
 	}
-	
 	//</Login>
 	
 	//<Communities/>
 	private static void communityMenu(){
 		String options[] = {"Minhas comunidades", "Criar comunidade", "Adicionar comunidade", "Todas as comunidades", "Voltar"};
-		clearScreen();
 		while(true){
+			clearScreen();
 			showTitle("Comunidades");
 			switch(displayMenuOptions(options)){
 			case 1:
@@ -309,7 +319,6 @@ public class iFace{
 			default:
 				return;
 			}
-			clearScreen();
 		}
 	}
 	private static void showAllCommunities(){
@@ -368,7 +377,7 @@ public class iFace{
 		clearScreen();
 		showTitle("Adicionar comunidade");
 		String cmntName = askLine("Nome da comunidade");
-		if(databaseFind(communities, cmntName, CMNT_NAME, MAX_CMNT) == -1){
+		if(databaseFind(communities, cmntName, CMNT_NAME, MAX_CMNT) != -1){
 			String items[] = {cmntName};
 			databaseInsert(currUserCmnt, items, items.length, MAX_CMNT);
 			System.out.println("Comunidade adicionada");
@@ -387,27 +396,103 @@ public class iFace{
 		System.out.println("------------------");
 	}
 	//</Communities>
-
+	
+	//<Friends/>
+	private static void friendsMenu(){
+		String options[] = {"Meus amigos", "Adicionar amigo", "Voltar"};
+		while(true){
+			clearScreen();
+			showTitle("Amigos");
+			switch(displayMenuOptions(options)){
+			case 1:
+				printDatabase(currUserFriends, FRIEND_COL, MAX_FRIENDS);
+				getchar();
+				break;
+			case 2:
+				clearScreen();
+				String email = askLine("E-mail do usuário");
+				if(databaseFind(accounts, email, ACCT_EMAIL, MAX_ACCT) != -1 && databaseFind(currUserFriends, email, FRIEND_EMAIL, MAX_FRIENDS) == -1 && !email.equals(currUserAcct[ACCT_EMAIL])){
+					addRequest(currUserAcct[ACCT_EMAIL], email, RQST_INVITE);
+					System.out.println("Solicitação de amizade enviada");
+					getchar();
+				}
+				else{
+					System.out.println("Usuário não encontrado");
+					getchar();
+				}
+				break;
+			default:
+				return;
+			}
+		}
+	}
+	//</Friends>
+	
+	//<Requests/>
+	private static void addRequest(String src, String dest, int type){
+		String items[] = {src, dest, Integer.toString(type)};
+		databaseInsert(requests, items, RQST_COL, MAX_RQST);
+	}
+	private static void solveRequests(){
+		for(int i = 0;i < MAX_RQST;i++){
+			if(requests[i][0] != null){
+				if(requests[i][RQST_DEST].equals(currUserAcct[ACCT_EMAIL])){
+					clearScreen();
+					switch(Integer.valueOf(requests[i][RQST_TYPE])){
+					case RQST_INVITE:
+						showTitle("Solicitação de amizade");
+						System.out.println(requests[i][RQST_SRC] + " te enviou uma solicitação de amizade");
+						String options[] = {"Aceitar", "Recusar"};
+						switch(displayMenuOptions(options)){
+						case 1:
+							addRequest(currUserAcct[ACCT_EMAIL], requests[i][RQST_SRC], RQST_INVITE_SUCCESS);
+							printDatabase(requests, RQST_COL, MAX_RQST);
+							String items[] = {requests[i][RQST_SRC]};
+							databaseInsert(currUserFriends, items, FRIEND_COL, MAX_FRIENDS);
+							break;
+						case 2:
+							addRequest(currUserAcct[ACCT_EMAIL], requests[i][RQST_DEST], RQST_INVITE_FAILURE);
+							break;
+						}
+						break;
+					case RQST_INVITE_SUCCESS:
+						System.out.println(requests[i][RQST_SRC] + " aceitou sua solicitação de amizade!");
+						String items[] = {requests[i][RQST_SRC]};
+						databaseInsert(currUserFriends, items, FRIEND_COL, MAX_FRIENDS);
+						break;
+					case RQST_INVITE_FAILURE:
+						System.out.println(requests[i][RQST_SRC] + " recusou sua solicitação de amizade");
+						break;
+					case RQST_ACCOUNT_CLOSURE:
+						databaseRemove(currUserFriends, requests[i][RQST_SRC], FRIEND_EMAIL, RQST_COL, MAX_RQST);
+						break;
+					}
+					for(int j = 0;j < RQST_COL;j++){
+						requests[i][j] = null;
+					}
+				}
+			}
+		}
+	}
+	//</Requests>
+	
+	//<Input/>
 	private static void getchar(){
 		System.out.println("\nAperte ENTER para continuar");
 		input.nextLine();
 	}
-	
 	private static int readInt(){
 		int read = input.nextInt();
 		input.nextLine();
 		return read;
-	}
-	
+	}	
 	private static String readLine(){
 		return input.nextLine();
 	}
-	
 	private static String askLine(String q){
 		System.out.print(q + ": ");
 		return readLine();
-	}
-	
+	}	
 	private static int displayMenuOptions(String items[]){
 		int selectedItem = 0;
 		
@@ -425,9 +510,15 @@ public class iFace{
 		
 		return selectedItem;
 	}
+	//</Input>
 	
 	private static void showTitle(String title){
 		System.out.println("\n" + title + "\n");
+	}
+	private static void clearScreen(){
+		for(int i = 0;i < 100;i++){
+			System.out.print("\n");
+		}
 	}
 	
 	private static void loadDatabase(String filename, String dest[][], int columns){
@@ -442,14 +533,13 @@ public class iFace{
 					line = br.readLine();
 				}
 			}
-
+			
 			br.close();
 		}
 		catch (IOException e){
 			e.printStackTrace();
 		}
 	}
-	
 	private static void printDatabase(String database[][], int columns, int max_rows){
 		String separator = "-----------";
 		for(int i = 0;i < max_rows;i++){
@@ -462,7 +552,6 @@ public class iFace{
 			}
 		}
 	}
-	
 	private static void saveDatabase(String filename, String source[][], int columns, int max_rows){
 		try{
 			File database = new File(path + filename);
@@ -482,13 +571,6 @@ public class iFace{
 			e.printStackTrace();
 		}
 	}
-	
-	private static void clearScreen(){
-		for(int i = 0;i < 90;i++){
-			System.out.print("\n");
-		}
-	}
-
 	private static void createFiles(String directory, String files[]){
 		for(int i = 0;i < files.length;i++){
 			try{
@@ -502,7 +584,7 @@ public class iFace{
 	}
 	private static int databaseFind(String database[][], String key, int column, int max_rows){
 		for(int i = 0;i < max_rows;i++){
-			if(database[i][column] != null){
+			if(database[i][0] != null){
 				if(database[i][column].equals(key)){
 					return i;
 				}
@@ -517,6 +599,17 @@ public class iFace{
 					database[i][j] = items[j];
 				}
 				return;
+			}
+		}
+	}
+	private static void databaseRemove(String database[][], String key, int column, int columns, int max_rows){
+		for(int i = 0;i < max_rows;i++){
+			if(database[i][0] != null){
+				if(database[i][column].equals(key)){
+					for(int j = 0;j < columns;j++){
+						database[i][j] = null;
+					}
+				}
 			}
 		}
 	}
